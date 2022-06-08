9e487212741f37ef638247fae4f828e6e32ebf8b
CRISTHIAN VILLEGAS GARCIA <cristianvg9692@gmail.com>
2022-06-07 19:00:00 -0500

    JUNIT5

--- src/test/java/org/cvg/junit5_app/ejemplo/models/CuentaTest.java
+++ src/test/java/org/cvg/junit5_app/ejemplo/models/CuentaTest.java
@@ -1,7 +1,6 @@
 package org.cvg.junit5_app.ejemplo.models;
 
 import org.cvg.junit5_app.ejemplo.exceptions.InsuficientMoneyException;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -24,18 +23,22 @@ class CuentaTest {
         //cuenta.setPersona("CRISTHIAN VILLEGAS");
         String valorEsperado = "CRISTHIAN VILLEGAS";
         String valorReal = this.cuenta.getPersona();
-        assertNotNull(valorReal);
-        assertEquals(valorEsperado, valorReal);
-        assertTrue(valorReal.equals(valorEsperado));
+        assertAll(
+                () -> assertNotNull(valorReal, () -> "El valor no puede ser nulo"),
+                () -> assertEquals(valorEsperado, valorReal, () -> "El valor esperado no es igual al valor real"),
+                () -> assertTrue(valorReal.equals(valorEsperado), () -> "Los valores son diferentes...")
+        );
     }
 
     @DisplayName("Test para comprobar el saldo de la cuenta")
     @Test
     void testSaldoCuenta() {
-        assertEquals(1000.12345, this.cuenta.getSaldo().doubleValue());
-        assertFalse(cuenta.getSaldo().compareTo( BigDecimal.ZERO ) < 0);
-        assertTrue(cuenta.getSaldo().compareTo( BigDecimal.ZERO ) > 0);
-        assertNotNull(cuenta.getSaldo());
+        assertAll(
+                () -> assertEquals(1000.12345, this.cuenta.getSaldo().doubleValue(), () -> "El saldo no es igual al esperado"),
+                () -> assertFalse(cuenta.getSaldo().compareTo( BigDecimal.ZERO ) < 0, () -> "El saldo no debe ser menor a 0"),
+                () -> assertTrue(cuenta.getSaldo().compareTo( BigDecimal.ZERO ) > 0, () -> "El saldo debe ser mayor a 0"),
+                () -> assertNotNull(cuenta.getSaldo(), () -> "El saldo no debe ser nulo")
+        );
     }
 
     /**
@@ -47,25 +50,29 @@ class CuentaTest {
         Cuenta cuenta2 = new Cuenta("Jhon Doe", new BigDecimal("8900.9997"));
 
 //        assertNotEquals(cuenta, cuenta2);
-        assertEquals(cuenta, cuenta2);
+        assertEquals(cuenta, cuenta2, () -> "Las cuentas no son iguales");
     }
 
     @Test
     void testDebitoCuenta() {
         Cuenta cuenta = new Cuenta("CRISTHIAN", new BigDecimal("1000.12345"));
         cuenta.debito(new BigDecimal(100));
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(900, cuenta.getSaldo().intValue());
-        assertEquals("900.12345", cuenta.getSaldo().toPlainString());
+        assertAll(
+                () -> assertNotNull(cuenta.getSaldo(), () -> "El saldo no debe ser nulo"),
+                () -> assertEquals(900, cuenta.getSaldo().intValue(), "El valor esperado no es igual al recibido"),
+                () -> assertEquals("900.12345", cuenta.getSaldo().toPlainString(), () -> "El valor esperado no es igual al recibido")
+        );
     }
 
     @Test
     void testCreditoCuenta() {
         Cuenta cuenta = new Cuenta("CRISTHIAN", new BigDecimal("1000.12345"));
         cuenta.credito(new BigDecimal(100));
-        assertNotNull(cuenta.getSaldo());
-        assertEquals(1100, cuenta.getSaldo().intValue());
-        assertEquals("1100.12345", cuenta.getSaldo().toPlainString());
+        assertAll(
+                () -> assertNotNull(cuenta.getSaldo(), () -> "El saldo no debe ser null"),
+                () -> assertEquals(1100, cuenta.getSaldo().intValue(), () -> "El valor recibido es diferente al esperado"),
+                () -> assertEquals("1100.12345", cuenta.getSaldo().toPlainString(), () -> "El valor recibido es diferente al esperado")
+        );
     }
 
     @Test
